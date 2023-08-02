663115ef4cf59f35111e82c50e08195d3a11c5df
Baily locatelli <bailylocatelli@gmail.com>
2023-08-01 20:37:05 -0700

    finished the js files, need to find bug with header

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -20,7 +20,7 @@ const tx = contactDb.transaction('jate', 'readwrite');
 
 const store = tx.objectStore('jate');
 
-const request = store.add({  });
+const request = store.add({ value: content });
 
 const result = await request;
 console.log('🚀 - data saved to the database', result);
